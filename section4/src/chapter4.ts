35202e377bbd7cf811ba0710cf60c64f942453fb
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-10-08 00:45:34 +0900

    section4 - chapter4

---
+++ section4/src/chapter4.ts
@@ -0,0 +1,33 @@
+// 사용자 정의 타입가드
+
+type Dog = {
+  name: string;
+  isBark: boolean;
+};
+
+type Cat = {
+  name: string;
+  isScratch: boolean;
+};
+
+type Animal = Dog | Cat;
+
+// 함수의 반환값으로는 어려움, 사용자 정의 타입가드 해줌
+function isDog(animal: Animal): animal is Dog {
+  return (animal as Dog).isBark !== undefined; // 타입 단언 사용해주기
+}
+
+function isCat(animal: Animal): animal is Cat {
+  return (animal as Cat).isScratch   !== undefined; // 타입 단언 사용해주기
+}
+
+function warning(animal: Animal) {
+  if (isDog(animal)) {
+    animal;
+  }
+  // if("isBark" in animal) {
+  //   console.log(animal.isBark);
+  // } else {
+  //   console.log(animal.isScratch);
+  // }
+}
